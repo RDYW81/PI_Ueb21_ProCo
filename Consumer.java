f97935a5735353cb326fdafc5a611e607e3594e0
RDYW81 <r.daidone@t-online.de>
2018-06-15 08:39:03 +0200

    JavaDoc

--- Consumer.java
+++ Consumer.java
@@ -9,18 +9,28 @@ import java.util.*;
 public class Consumer
 {
 
-    //HashMap-Register
+    //TreeMap-Register
     TreeMap<Integer, ArrayList<Long>> protokoll = new TreeMap<Integer, ArrayList<Long>>();
 
     /**
-     * Berechnet Quersumme...
+     * Diese Methode nimmt einen Integer entgegen und berechnet die Quersumme.
+     * Für jede Quersumme wird ein Zeitstempel der Berechnung erstellt. Jeder
+     * Zeitstempel (= Protokoll) wird in einer ArrayList (= occurrences) gespeichert. 
+     * Die occurrences werden wiederrum in einer TreeMap (= Register) hinterlegt,
+     * um effizient darauf zugreifen zu können.
+     * 
+     * Wenn noch kein Protokoll mit einer errechneten Quersumme angelegt wurde,
+     * dann wird in der ArrayList ein neues Protokoll mit Zeitstempel angelegt.
+     * 
+     * @param zahl  zu uebergebende Zahl
+     * 
      */
     public void consume(int zahl) {
         int quersumme = 0;
         while (0 != zahl) {
-            // addiere die letzte ziffer der uebergebenen zahl zur summe
+            // Addiert die letzte Ziffer der uebergebenen Zahl zur Quersumme
             quersumme = quersumme + (zahl % 10);
-            // entferne die letzte ziffer der uebergebenen zahl
+            // Entfernt die letzte Ziffer der uebergebenen Zahl
             zahl = zahl / 10;
         }       
 
@@ -32,28 +42,37 @@ public class Consumer
         // protokoll.get(quersumme).add(System.currentTimeMillis());
     }
 
-    //numberOfDifferentResults
+    /** 
+     * Diese Methode gibt an, wie viele unterschiedliche Quersummen berechnet wurden.
+     * 
+     * @return Anzahl unterschiedlicher Quersummen
+     */
     public int numberOfDifferentResults() {
         return protokoll.size();
     }
 
-    //numberOfOccurrences gibt Einträge zurück
+    /** 
+     * Diese Methode gibt an, wie viele Eintraege zu einer entsprechenden Quersummen existieren.
+     * 
+     * @param quersumme  zu übergebende Quersumme
+     * @return Anzahl der Eintraege zu einer bestimmten Quersumme
+     */
     public int numberOfOccurrences(int quersumme) {
         ArrayList<Long> occurrences = protokoll.get(quersumme);
         if (occurrences == null)
             return 0;
-            
+
         return occurrences.size();
     }
-    
+
     public Set<Integer> getCrossTotalsAscending() {
         return protokoll.keySet();
     }
-    
+
     public Set<Integer> getCrossTotalsDescending() {
         return protokoll.descendingKeySet();
     }
-    
+
     public ArrayList<Long> getTimestampsForResult(int quersumme) {
         return protokoll.get(quersumme);
     }
