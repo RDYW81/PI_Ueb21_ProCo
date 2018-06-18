7c3866ecaf302058e43c34eb18328aa62d309392
RDYW81 <r.daidone@t-online.de>
2018-06-18 16:41:05 +0200

    Producer in Anfangszustand versetzt.

--- Producer.java
+++ Producer.java
@@ -8,26 +8,13 @@ import java.util.*;
  */
 public class Producer
 {
-    // Instanzvariablen
-    // Random ran;
-
-    // /**
-    // * Konstruktor für Objekte der Klasse Consumer
-    // */
-    // public Producer()
-    // {
-    // // Instanzvariable initialisieren
-    // ran = new Random();
-    // }
-
     /**
      * Erzeugt einen zufälligen Integer im geschlossenen Intervall [0 - 1000] und gibt diesen zurück.
      * 
-     * @return randomNummer gibt Integer zurück
+     * @return ran.nextInt gibt Integer zurück
      */
     public int produce() {
         Random ran = new Random();
-        // int randomNummer = 1000; //ran.nextInt(1001);
-        return ran.nextInt(1001); // randomNummer;
+        return ran.nextInt(1001); 
     }
 }
